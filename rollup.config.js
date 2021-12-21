c17f9724cd8b46abd38a3520b37472e6a295410e
chejen <jkopre.qek@gmail.com>
2021-12-21 23:16:31 +0800

    hello

--- rollup.config.js
+++ rollup.config.js
@@ -17,7 +17,7 @@ function serve() {
 	return {
 		writeBundle() {
 			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+			server = require('child_process').spawn('npm', ['run', 'start:server', '--', '--dev'], {
 				stdio: ['ignore', 'inherit', 'inherit'],
 				shell: true
 			});
